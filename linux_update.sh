f62a1e5c11f1d09ec538848d77e3eded15b4d559
BigBlackDog <github.com@uwe-husmann.de>
2020-10-30 09:03:24 +0100

    added zshrc and aliases to a new shell-section

--- linux_update.sh
+++ linux_update.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 cp --verbose neovim/.config/nvim/init.vim $HOME/.config/nvim/
+cp --verbose i3/config $HOME/.i3/
+cp --verbose shells/zshrc $HOME/.zshrc
+cp --verbose shells/aliases $HOME/.aliases

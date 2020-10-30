4a75237168bf64c55952a08584c84a90149aabf3
BigBlackDog <github.com@uwe-husmann.de>
2020-10-30 19:24:08 +0100

    added tmux.conf

--- linux_update.sh
+++ linux_update.sh
@@ -4,3 +4,4 @@ cp --verbose neovim/.config/nvim/init.vim $HOME/.config/nvim/
 cp --verbose i3/config $HOME/.i3/
 cp --verbose shells/zshrc $HOME/.zshrc
 cp --verbose shells/aliases $HOME/.aliases
+cp --verbose tmux/tmux.conf $HOME/.tmux.conf

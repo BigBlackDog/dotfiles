23861ddd96c36270577f8651c3ca6e5dc3ac8448
Uwe <uh@digga.fritz.box>
2021-06-24 17:03:52 +0200

    copy nvim-configuration recursively on linux

--- linux_update.sh
+++ linux_update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-cp --verbose neovim/.config/nvim/init.vim $HOME/.config/nvim/
+mkdir -p $HOME/.config/nvim
+cp --verbose -r neovim/.config/nvim/* $HOME/.config/nvim/
+
+mkdir -p $HOME/.i3
 cp --verbose i3/config $HOME/.i3/
+
 cp --verbose shells/zshrc $HOME/.zshrc
 cp --verbose shells/aliases $HOME/.aliases
 cp --verbose tmux/tmux.conf $HOME/.tmux.conf
+
+mkdir -p $HOME/.config/alacritty
 cp --verbose alacritty/.config/alacritty/alacritty.yml $HOME/.config/alacritty/
 cp --verbose picom/.config/picom.conf $HOME/.config/

aac987fc7641a042d21412987c52fa9f4dcdb90c
BigBlackDog <github.com@uwe-husmann.de>
2022-01-20 23:27:40 +0100

    Use the same keymaps for all languages using LSP-Servers

--- neovim/.config/nvim/lua/my-lsp-stuff.lua
+++ neovim/.config/nvim/lua/my-lsp-stuff.lua
@@ -1,26 +1,27 @@
 -- Native LSP-Setup
--- TODO: make mappings all the same for LSPs
 -- TODO: explore :Telescope lsp_references and the likes ...
 
 -- this has to be done before setting up the LSP-Servers
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
+-- setup keys the same way for all languages
+local function setupKeymaps()
+   vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0}) --in "n"ormalmode, on pressing "K", call vim.ls..., on the current buffer
+   vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0}) --in "n"ormalmode, on pressing "gd", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
+   vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0}) --in "n"ormalmode, on pressing "gt", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
+   vim.keymap.set("n", "gm", vim.lsp.buf.implementation, {buffer=0}) --in "n"ormalmode, on pressing "gm", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
+   vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0}) --in "n"ormalmode, on pressing "<leader>df", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
+   vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer=0}) --in "n"ormalmode, on pressing "<leader>dl", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
+   -- note: use <c-q> to populate to quickfix list
+   vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0}) --in "n"ormalmode, on pressing "<leader>db", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
+   vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, {buffer=0}) --in "n"ormalmode, on pressing "<leader>rr", call vim.ls..., on the current buffer
+end
 -- GO
 -- 1. get gopls
 -- 2. tell nvim to use it
 require'lspconfig'.gopls.setup{
     capabilities = capabilities,
-    on_attach = function()
-       vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0}) --in "n"ormalmode, on pressing "K", call vim.ls..., on the current buffer
-       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0}) --in "n"ormalmode, on pressing "gd", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
-       vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0}) --in "n"ormalmode, on pressing "gt", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
-       vim.keymap.set("n", "gm", vim.lsp.buf.implementation, {buffer=0}) --in "n"ormalmode, on pressing "gm", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
-       vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer=0}) --in "n"ormalmode, on pressing "<leader>df", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
-       vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer=0}) --in "n"ormalmode, on pressing "<leader>dl", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
-       -- note: use <c-q> to populate to quickfix list
-       vim.keymap.set("n", "<leader>db", vim.diagnostic.goto_prev, {buffer=0}) --in "n"ormalmode, on pressing "<leader>db", call vim.ls..., on the current buffer, <c-t> brings you back (:help tagstack)
-       vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, {buffer=0}) --in "n"ormalmode, on pressing "<leader>rr", call vim.ls..., on the current buffer
-    end,
+    on_attach = setupKeymaps,
 } -- connect to server
 
 -- Elixir
@@ -32,7 +33,5 @@ require'lspconfig'.gopls.setup{
 -- mix elixir_ls.release -o release
 require'lspconfig'.elixirls.setup{
     cmd = { vim.loop.os_homedir().."/git-repos/running/elixir-ls/release/language_server.sh" };
-    on_attach = function()
-       vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0}) --in "n"ormalmode, on pressing "K", call vim.ls..., on the current buffer
-    end,
+    on_attach = setupKeymaps,
 }

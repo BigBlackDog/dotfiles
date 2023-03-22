75bd7d06f0db4ff022e144fcc9f415da2971764f
BigBlackDog <github.com@uwe-husmann.de>
2023-03-22 21:09:47 +0100

    Removed duplicate options and mappings. Harpoon maps fixed.

--- neovim/.config/nvim/lua/custom/maps.lua
+++ neovim/.config/nvim/lua/custom/maps.lua
@@ -1,15 +1,3 @@
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- [[ Very useful keymaps that won't fit anywhere else ]]
--- don't bend your fingers
-vim.keymap.set({ 'i' }, 'jj', '<ESC>', { silent = true })
-
 -- move visual blocks around
 vim.keymap.set({ 'v' }, 'J', ":m '>+1<CR>gv=gv", { noremap = true })
 vim.keymap.set({ 'v' }, 'K', ":m '<-2<CR>gv=gv", { noremap = true })
@@ -28,13 +16,13 @@ vim.keymap.set({ 'n' }, '<A-k>', '<C-w>K', { noremap = true })
 vim.keymap.set({ 'n' }, '<A-l>', '<C-w>L', { noremap = true })
 
 -- [[ harpooning ]]
-vim.keymap.set('n', '<leader>ha', require('harpoon.mark').add_file, { desc = '[H]arpoon [A]dd_file' })
-vim.keymap.set('n', '<leader>hh', require('harpoon.ui').toggle_quick_menu, { desc = '[H]arpoon s[H]ow_menu' })
-vim.keymap.set('n', '<leader>hf', function() return require('harpoon.ui').nav_file(1) end, { desc = '[H]arpoon navigate to first file[F]' })
-vim.keymap.set('n', '<leader>hd', function() return require('harpoon.ui').nav_file(2) end, { desc = '[H]arpoon navigate to second file[D]' })
-vim.keymap.set('n', '<leader>hs', function() return require('harpoon.ui').nav_file(3) end, { desc = '[H]arpoon navigate to third file[S]' })
-vim.keymap.set('n', '<leader>hu', require('harpoon.ui').nav_next, { desc = '[H]arpoon navigate next [U]' })
-vim.keymap.set('n', '<leader>hd', require('harpoon.ui').nav_prev, { desc = '[H]arpoon navigate prev [D]' })
+vim.keymap.set({ 'n' }, '<leader>ha', require('harpoon.mark').add_file, { desc = '[H]arpoon [A]dd_file' })
+vim.keymap.set({ 'n' }, '<leader>hh', require('harpoon.ui').toggle_quick_menu, { desc = '[H]arpoon s[H]ow_menu' })
+vim.keymap.set({ 'n' }, '<leader>hf', function() return require('harpoon.ui').nav_file(1) end, { desc = '[H]arpoon navigate to first file[F]' })
+vim.keymap.set({ 'n' }, '<leader>hd', function() return require('harpoon.ui').nav_file(2) end, { desc = '[H]arpoon navigate to second file[D]' })
+vim.keymap.set({ 'n' }, '<leader>hs', function() return require('harpoon.ui').nav_file(3) end, { desc = '[H]arpoon navigate to third file[S]' })
+vim.keymap.set({ 'n' }, '<leader>hu', require('harpoon.ui').nav_next, { desc = '[H]arpoon navigate next [U]' })
+vim.keymap.set({ 'n' }, '<leader>hr', require('harpoon.ui').nav_prev, { desc = '[H]arpoon navigate prev [R]' })
 
 -- [[ Buffer handling - old habits die hard ]]
 vim.keymap.set({ 'n' }, '<Leader>bd', ':bd<CR>', {})
--- neovim/.config/nvim/lua/custom/options.lua
+++ neovim/.config/nvim/lua/custom/options.lua
@@ -6,9 +6,8 @@ vim.opt.cmdheight = 1 -- Height of the command bar
 vim.opt.incsearch = true -- Makes search act like search in modern browsers
 vim.opt.showmatch = true -- show matching brackets when text indicator is over them
 vim.opt.relativenumber = true -- Show line numbers
+-- in kickstart they use vim.wo.number = true ... i don't know the difference
 vim.opt.number = true -- But show the actual number for the line we're on
-vim.opt.ignorecase = true -- Ignore case when searching...
-vim.opt.smartcase = true -- ... unless there is a capital letter in the query
 
 vim.opt.hidden = true -- I like having buffers stay around
 vim.opt.equalalways = false -- I don't like my windows changing all the time
@@ -29,7 +28,6 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
-vim.opt.breakindent = true
 vim.opt.showbreak = string.rep(" ", 3) -- Make it so that long lines wrap smartly
 vim.opt.linebreak = true
 
@@ -39,24 +37,11 @@ vim.opt.modelines = 1
 
 vim.opt.belloff = "all" -- Just turn the dang bell off
 
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
 vim.opt.diffopt = { "internal", "filler", "closeoff", "hiddenoff", "algorithm:minimal" }
-
--- Save undo history
-vim.o.undofile = true
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.cmd.colorscheme('ayu-mirage')
-vim.o.termguicolors = true
 
 -- Set completeopt to have a better completion experience
+-- preview is additional to kickstart ... have to read up on the difference again
 vim.o.completeopt =  "menuone,noselect,preview"

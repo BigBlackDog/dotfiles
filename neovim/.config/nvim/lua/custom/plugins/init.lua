aeb15ec1fa46ec6b3944a9417489e82fe44d2646
BigBlackDog <github.com@uwe-husmann.de>
2023-03-29 22:05:48 +0200

    fix non-autoloading plugins for LazyVim

--- neovim/.config/nvim/lua/custom/plugins/init.lua
+++ neovim/.config/nvim/lua/custom/plugins/init.lua
@@ -1,8 +1,8 @@
 return
   {
     {'ThePrimeagen/harpoon', lazy = true},
-    {'kyazdani42/nvim-web-devicons', lazy = true},
     {'elixir-editors/vim-elixir', lazy = true},
-    {'shushcat/vim-minimd', lazy = true},
-    {'tjdevries/cyclist.vim', lazy = true},
+    {'kyazdani42/nvim-web-devicons'},
+    {'shushcat/vim-minimd'},
+    {'tjdevries/cyclist.vim'},
   }

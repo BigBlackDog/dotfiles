c9e6ead7ad947f66e4c0240d076ecc1427ed6f1f
Uwe Husmann <Uwe Husmann>
2024-02-19 19:45:36 +0100

    fixed harpoon maps

--- neovim/.config/nvim/lua/custom/maps.lua
+++ neovim/.config/nvim/lua/custom/maps.lua
@@ -9,13 +9,13 @@ vim.keymap.set({ 'n' }, '<A-k>', '<C-w>K', { noremap = true })
 vim.keymap.set({ 'n' }, '<A-l>', '<C-w>L', { noremap = true })
 
 -- [[ harpooning ]]
-vim.keymap.set({ 'n' }, '<leader>ha', require('harpoon.mark').add_file, { desc = '[H]arpoon [A]dd_file' })
-vim.keymap.set({ 'n' }, '<leader>hh', require('harpoon.ui').toggle_quick_menu, { desc = '[H]arpoon s[H]ow_menu' })
-vim.keymap.set({ 'n' }, '<leader>hf', function() return require('harpoon.ui').nav_file(1) end, { desc = '[H]arpoon navigate to first file[F]' })
-vim.keymap.set({ 'n' }, '<leader>hd', function() return require('harpoon.ui').nav_file(2) end, { desc = '[H]arpoon navigate to second file[D]' })
-vim.keymap.set({ 'n' }, '<leader>hs', function() return require('harpoon.ui').nav_file(3) end, { desc = '[H]arpoon navigate to third file[S]' })
-vim.keymap.set({ 'n' }, '<C-j>', require('harpoon.ui').nav_next, { desc = '[H]arpoon navigate next [U]' })
-vim.keymap.set({ 'n' }, '<C-k>', require('harpoon.ui').nav_prev, { desc = '[H]arpoon navigate prev [R]' })
+vim.keymap.set({ 'n' }, '<leader>ha', function() harpoon:list():append() end, { desc = '[H]arpoon [A]dd_file' })
+vim.keymap.set({ 'n' }, '<leader>hh', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = '[H]arpoon s[H]ow_menu' })
+vim.keymap.set({ 'n' }, '<leader>hf', function() harpoon:list():select(1) end, { desc = '[H]arpoon navigate to first file[F]' })
+vim.keymap.set({ 'n' }, '<leader>hd', function() harpoon:list():select(2) end, { desc = '[H]arpoon navigate to second file[D]' })
+vim.keymap.set({ 'n' }, '<leader>hs', function() harpoon:list():select(3) end, { desc = '[H]arpoon navigate to third file[S]' })
+vim.keymap.set({ 'n' }, '<C-j>', function() harpoon:list():next() end, { desc = '[H]arpoon navigate next [U]' })
+vim.keymap.set({ 'n' }, '<C-k>', function() harpoon:list():prev() end, { desc = '[H]arpoon navigate prev [R]' })
 
 -- [[ Buffer handling - old habits die hard ]]
 vim.keymap.set({ 'n' }, '<Leader>bd', ':bd<CR>', {})
